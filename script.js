a5e2c0c21c6228e71433d0c50563db852f49e576
enzotoms <thomaschristo113@gmail.com>
2024-11-13 10:49:18 +0100

    worked on the file again

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const quizData = [
         a: "Java",
         b: "C",
         c: "Python",
-        d: "javascript",
+        d: "JavaScript",
         correct: "d",
     },
     {
@@ -78,70 +78,112 @@ const quizData = [
         c: "To manage user interfaces",
         d: "To manage database connections",
         correct: "b",
+    },
+    {
+        question: "What keyword is used to define a class in Python?",
+        a: "function",
+        b: "def",
+        c: "class",
+        d: "struct",
+        correct: "c",
+    },
+    {
+        question: "What is the result of 3+4*5 in Python?",
+        a: "35",
+        b: "23",
+        c: "20",
+        d: "17",
+        correct: "b",
+    },
+    {
+        question: "In C++, what is the correct syntax to print a value to the console?",
+        a: "print(value);",
+        b: "console.log(value);",
+        c: "cout << value;",
+        d: "printf(value);",
+        correct: "c",
+    },
+    {
+        question: "In React, what is a component?",
+        a: "A function or class that returns a piece of the UI",
+        b: "A database model",
+        c: "An HTTP request handler",
+        d: "A styling library",
+        correct: "a",
+    },
+    {
+        question: "In C#, what keyword is used to inherit from a base class?",
+        a: "extends",
+        b: "inherits",
+        c: "implements",
+        d: "base",
+        correct: "a",
     }
 ];
 
+const quiz = document.getElementById('quiz')
+ const answerEls = document.querySelectorAll('.answer')
+  const questionEl = document.getElementById('question')
+   const a_text = document.getElementById('a_text')
+    const b_text = document.getElementById('b_text')
+     const c_text = document.getElementById('c_text')
+      const d_text = document.getElementById('d_text')
+       const submitBtn = document.getElementById('submit')
+        const result = document.getElementById('result')
+         let currentQuiz = 0 
+let score = 0 
+loadQuiz() 
 
-const quiz= document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById('submit')
+function loadQuiz() 
+{ deselectAnswers() 
 
+    const currentQuizData = quizData[currentQuiz] 
+    questionEl.innerText = currentQuizData.question 
+    a_text.innerText = currentQuizData.a 
+    b_text.innerText = currentQuizData.b 
+    c_text.innerText = currentQuizData.c 
+    d_text.innerText = currentQuizData.d 
+    result.classList.remove('correct', 'wrong') 
+    result.innerText = '' 
+} 
 
-let currentQuiz = 0
-let score = 0
+    function deselectAnswers() { 
+        answerEls.forEach(answerEl => answerEl.checked = false)
+     } 
 
-loadQuiz()
+     function getSelected() { 
+        let answer
+         answerEls.forEach(answerEl => { 
+            if (answerEl.checked) { 
+                answer = answerEl.id
+             }
+             })
 
-function loadQuiz() {
+                 return answer 
+                } 
+                
+                submitBtn.addEventListener('click', () => { 
+                    const answer = getSelected()
+                     if (answer) {
+                         if (answer === quizData[currentQuiz].correct)
+                             { 
+                        score++ 
+                        result.innerText = 'Correct!' 
+                        result.classList.add('correct')
+                     } else {
+                         result.innerText = `Wrong answer! The correct answer was ${quizData[currentQuiz].correct.toUpperCase()}.`
+                      result.classList.add('wrong')
+                     }
+                     
+                     currentQuiz++ 
 
-    deselectAnswers()
+                     if (currentQuiz < quizData.length) {
+                         setTimeout(loadQuiz, 1000) // Delay to show the result message before loading the next question
+                          } else { 
+                            quiz.innerHTML = `
+                             <h2>You answered ${score}/${quizData.length} questions correctly</h2>
 
-    const currentQuizData = quizData[currentQuiz]
-
-    questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
-}
-
-function deselectAnswers() {
-    answerEls.forEach(answerEl => answerEl.checked = false)
-}
-
-function getSelected() {
-    let answer
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
-            answer = answerEl.id
-        }
-    })
-    return answer
-}
-
-
-submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
-    if(answer) {
-       if(answer === quizData[currentQuiz].correct) {
-           score++
-       }
-
-       currentQuiz++
-
-       if(currentQuiz < quizData.length) {
-           loadQuiz()
-       } else {
-           quiz.innerHTML = `
-           <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-
-           <button onclick="location.reload()">Reload</button>
-           `
-       }
-    }
-})
+                              <button onclick="location.reload()">Reload</button>
+                               ` } 
+                            }
+                        })
